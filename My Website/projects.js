900f21e4251f61578bca429679c7314ac713ccd9
ishmaelewis40 <84811957+ishmaelewis40@users.noreply.github.com>
2021-09-01 11:36:30 +0100

    Add files via upload

---
+++ My Website/projects.js
@@ -0,0 +1,9 @@
+//paralax
+const parallax = document.getElementById
+("parallax");
+
+window.addEventListener("scroll", function()
+{
+    let offset = window.pageYOffset;
+    console.log('offset: ' + offset);
+})
